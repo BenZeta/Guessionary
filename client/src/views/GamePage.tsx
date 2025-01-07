cb081cd000b5a43c0a07a7a922cf27a50a810a6a
BenZeta <benpardede.hacktiv8@gmail.com>
2025-01-07 22:15:02 +0700

    test

--- client/src/views/GamePage.tsx
+++ client/src/views/GamePage.tsx
@@ -11,9 +11,7 @@ export default function GamePage() {
         {/* Left Panel: Room List */}
         <div className="w-3/12 bg-white/10 p-4">
           <div className="bg-black bg-opacity-10 p-5 rounded-lg h-full flex flex-col">
-            <h2 className="text-xl font-bold text-teal-300 mb-4 flex justify-center">
-              Player
-            </h2>
+            <h2 className="text-xl font-bold text-teal-300 mb-4 flex justify-center">Player</h2>
             <div className="h-[calc(100%-100px)] overflow-y-auto flex flex-col gap-4 scrollbar p-1">
               <div className="p-4 bg-black/20 text-white rounded-lg cursor-pointer flex items-center gap-3 ">
                 <img
@@ -91,18 +89,14 @@ export default function GamePage() {
             </div>
 
             {/* Create Room Button */}
-            <button className="mt-4 bg-teal-500 hover:bg-teal-600 text-white font-semibold py-2 px-4 rounded-md shadow-lg">
-              Create New Room
-            </button>
+            <button className="mt-4 bg-teal-500 hover:bg-teal-600 text-white font-semibold py-2 px-4 rounded-md shadow-lg">Create New Room</button>
           </div>
         </div>
 
         {/* Right Panel: Profile */}
         <div className="w-9/12 bg-white/10 p-4">
           <div className="bg-black bg-opacity-10 p-5 rounded-lg h-full flex flex-col">
-            <h2 className="text-xl font-bold text-teal-300 mb-4 flex justify-center">
-              Your Profile
-            </h2>
+            <h2 className="text-xl font-bold text-teal-300 mb-4 flex justify-center">Your Profile</h2>
 
             {/* Grid Content */}
             <div className="gap-5 rounded-lg w-full h-full overflow-y-auto scrollbar flex-1 p-1">
