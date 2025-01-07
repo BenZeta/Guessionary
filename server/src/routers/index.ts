fc7c028870c24c928bf0e8e29996eee810a1b59c
BenZeta <benpardede.hacktiv8@gmail.com>
2025-01-07 15:17:12 +0700

    feat: authentication & socket room almost done

--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
 import AuthController from '../controllers/AuthController';
+import RoomController from '../controllers/RoomController';
+import { authentication } from '../middlewares/authentication';
 const router = Router();
 
 router.post('/login', AuthController.login);
+
+router.use(authentication);
+router.post('/create-room', RoomController.createRoom);
 
 export default router;
