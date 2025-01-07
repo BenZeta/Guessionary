0e2c306daa2001d299328dc8a4f86aedc01ff3bf
Nazmi <nouvalnazmi2005@gmail.com>
2025-01-07 12:47:48 +0700

    for pull

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,27 +2,6 @@ import express from 'express';
 import router from './routers';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-
-interface ServerToClientEvents {
-  noArg: () => void;
-  basicEmit: (a: number, b: string, c: Buffer) => void;
-  withAck: (d: string, callback: (e: number) => void) => void;
-}
-
-interface ClientToServerEvents {
-  hello: () => void;
-}
-
-interface InterServerEvents {
-  ping: () => void;
-}
-
-interface SocketData {
-  name: string;
-  age: number;
-}
 
 if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
@@ -31,23 +10,12 @@ if (process.env.NODE_ENV !== 'production') {
 const app = express();
 const port = process.env.PORT || 8080;
 
-const httpServer = createServer(app);
-const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(httpServer, {
-  cors: {
-    origin: 'http://localhost:5173',
-  },
-});
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: '*' }));
 
 app.use(router);
 
-io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
-});
-
-httpServer.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
