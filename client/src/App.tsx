e374d38cb65e8117eee1f7e3f53e90935f1bdb69
BenZeta <benpardede.hacktiv8@gmail.com>
2025-01-07 23:12:19 +0700

    feat: merge homepage and animated features on game

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,20 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./views/HomePage";
 import LoginPage from "./views/LoginPage";
 import GamePage from "./views/GamePage";
-import Game2Page from "./views/Game2Page";
-import Game3Page from "./views/Game3Page";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route
+          index
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="/login"
+          element={<LoginPage />}
+        />
         <Route
           path="/game/:roomId"
           element={<GamePage />}
