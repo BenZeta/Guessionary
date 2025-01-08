9c0f967b96d97f8c073aebb90d9dd09edb8149d9
ferdianrickyagustine <ferdianrickyag@gmail.com>
2025-01-08 14:02:48 +0700

    feat: Navbar, setLoading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,24 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./views/HomePage";
 import LoginPage from "./views/LoginPage";
 import GamePage from "./views/GamePage";
+import Game2Page from "./views/Game2Page";
+import Game3Page from "./views/Game3Page";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          index
-          path="/"
-          element={<HomePage />}
-        />
-        <Route
-          path="/login"
-          element={<LoginPage />}
-        />
-        <Route
-          path="/game/:roomId"
-          element={<GamePage />}
-        />
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/game/:roomId" element={<GamePage />} />
+        <Route path="/game2" element={<Game2Page />} />
+        <Route path="/game3" element={<Game3Page />} />
       </Routes>
     </BrowserRouter>
   );
