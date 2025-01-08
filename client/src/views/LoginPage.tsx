643539d5688eddb8f24211d4ca2d751578f3c848
razaan24 <daffarazann@gmail.com>
2025-01-08 12:48:51 +0700

    feat avatar, username

--- client/src/views/LoginPage.tsx
+++ client/src/views/LoginPage.tsx
@@ -2,17 +2,23 @@ import axios from "axios";
 import { useState } from "react";
 import { baseUrl } from "../constants/baseUrl";
 import { useNavigate } from "react-router";
+import { Avatar } from "../helpers/avatar";
 
 export default function LoginPage() {
+  const [avatar, setAvatar] = useState<string>("");
   const [username, setUsername] = useState<string>("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post(baseUrl + "/login", { username });
+      const { data } = await axios.post(baseUrl + "/login", {
+        avatar,
+        username,
+      });
 
       localStorage.setItem("access_token", data.access_token);
+      localStorage.setItem("avatar", data.avatar);
       localStorage.setItem("username", data.username);
 
       navigate("/");
@@ -23,17 +29,13 @@ export default function LoginPage() {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          name="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+      <div>
+        <img
+          className="h-7"
+          src="https://ik.imagekit.io/3a0xukows/Guessionary%20v1.png?updatedAt=1736265436299"
+          alt="logo"
         />
-        <button type="submit">Submit</button>
-      </form>
+      </div>
     </div>
   );
 }
