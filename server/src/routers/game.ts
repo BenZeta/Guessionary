5965d11251bfb633830360a6170685a466c178b8
BenZeta <benpardede.hacktiv8@gmail.com>
2025-01-08 20:46:29 +0700

    feat: fix lobby game id for server

--- server/src/routers/game.ts
+++ server/src/routers/game.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import GameController from '../controllers/GameController';
 const router = Router();
 
+router.get('/', GameController.getGames);
 router.get('/start/:roomId/:gameId', GameController.startGame);
 
 export default router;
