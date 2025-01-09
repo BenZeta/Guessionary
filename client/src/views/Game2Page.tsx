f95803a4d49679c834bc51ffdcee05e2b76ef145
ferdianrickyagustine <ferdianrickyag@gmail.com>
2025-01-10 05:45:33 +0700

    Feat: added timer to other game pages (round2 round3)

--- client/src/views/Game2Page.tsx
+++ client/src/views/Game2Page.tsx
@@ -40,6 +40,8 @@ export default function Game2Page() {
   const [selectedColor, setSelectedColor] = useState<string>("#000000");
   const [brushSize, setBrushSize] = useState<number>(5);
   const [dataDrawing, setDataDrawing] = useState<string>("");
+  const [timer, setTimer] = useState<number>(60); // Timer for 15 seconds
+  const [isTimerRunning, setIsTimerRunning] = useState<boolean>(true);
   const { gameId, roomId } = useParams();
   const navigate = useNavigate();
 
@@ -76,6 +78,20 @@ export default function Game2Page() {
     };
   }, []);
 
+  // Timer logic
+  useEffect(() => {
+    if (isTimerRunning && timer > 0) {
+      const interval = setInterval(() => {
+        setTimer((prev) => prev - 1);
+      }, 1000);
+
+      return () => clearInterval(interval); // Cleanup interval on component unmount or when timer stops
+    } else if (timer === 0) {
+      setIsTimerRunning(false); // Stop the timer when it reaches 0
+      console.log("Time's up!");
+    }
+  }, [isTimerRunning, timer]);
+
   const getUser = async () => {
     try {
       const { data } = await axios.get(`${baseUrl}/users/${roomId}`, {
@@ -85,7 +101,9 @@ export default function Game2Page() {
       });
 
       setUsers(data.users);
-      const user = data.users.find((user: User) => user.id === localStorage.userId);
+      const user = data.users.find(
+        (user: User) => user.id === localStorage.userId
+      );
       console.log("User Role:", user.role); // Log to check user role
       socket.emit("userList", data?.users);
     } catch (error) {
@@ -199,29 +217,37 @@ export default function Game2Page() {
         {/* Left Panel */}
         <div className="w-3/12 bg-white/10 p-6">
           <div className="bg-black bg-opacity-10 p-5 rounded-lg h-full flex flex-col">
-            <h2 className="text-xl font-bold text-teal-300 mb-4 text-center">Player</h2>
+            <h2 className="text-xl font-bold text-teal-300 mb-4 text-center">
+              Player
+            </h2>
             <div className="h-[calc(100%-100px)] overflow-y-auto flex flex-col gap-4 scrollbar p-1">
               {users.map((user: User, index: number) => (
                 <div
                   key={index}
-                  className="p-4 bg-black/20 text-white rounded-lg cursor-pointer flex items-center gap-3 ">
-                  <img
-                    src={user?.avatar}
-                    className="w-20 h-20 rounded-full"
-                  />
+                  className="p-4 bg-black/20 text-white rounded-lg cursor-pointer flex items-center gap-3 "
+                >
+                  <img src={user?.avatar} className="w-20 h-20 rounded-full" />
                   <div className="ml-3 text-2xl">{user?.username}</div>
                 </div>
               ))}
             </div>
-            <button className="mt-4 bg-teal-500 hover:bg-teal-600 text-white font-semibold py-2 px-4 rounded-md shadow-lg">Create New Room</button>
+            <button className="mt-4 bg-teal-500 hover:bg-teal-600 text-white font-semibold py-2 px-4 rounded-md shadow-lg">
+              Create New Room
+            </button>
           </div>
         </div>
 
         {/* Right Panel */}
         <div className="w-9/12 bg-white/10 p-6">
           <div className="bg-black bg-opacity-10 p-5 rounded-lg h-full flex flex-col">
-            <h2 className="text-xl font-silkscreen font-bold text-teal-300 mb-4 text-center">Game</h2>
-
+            <h2 className="text-xl font-silkscreen font-bold text-teal-300 mb-4 text-center">
+              Game
+            </h2>
+            {isTimerRunning ? (
+              <div className="text-xl font-bold text-white mb-5">
+                Time remaining: {timer} seconds
+              </div>
+            ) : null}
             {/* Toolbar */}
             <div className="flex gap-4 mb-4 items-center">
               {COLORS.map((color) => (
@@ -229,7 +255,12 @@ export default function Game2Page() {
                   key={color}
                   onClick={() => setSelectedColor(color)}
                   style={{ backgroundColor: color }}
-                  className={`w-10 h-10 rounded-full cursor-pointer border-2 ${selectedColor === color ? "border-white" : "border-transparent"}`}></div>
+                  className={`w-10 h-10 rounded-full cursor-pointer border-2 ${
+                    selectedColor === color
+                      ? "border-white"
+                      : "border-transparent"
+                  }`}
+                ></div>
               ))}
               <input
                 type="range"
@@ -241,25 +272,26 @@ export default function Game2Page() {
               />
               <button
                 onClick={handleEraser}
-                className="bg-gray-500 text-white px-4 py-2 rounded-md">
+                className="bg-gray-500 text-white px-4 py-2 rounded-md"
+              >
                 Eraser
               </button>
 
               <button
                 onClick={handleClear}
-                className="bg-red-500 text-white px-4 py-2 rounded-md">
+                className="bg-red-500 text-white px-4 py-2 rounded-md"
+              >
                 Clear
               </button>
             </div>
 
-            <div
-              className="flex-1 bg-white relative"
-              ref={containerRef}>
+            <div className="flex-1 bg-white relative" ref={containerRef}>
               <canvas ref={canvasRef}></canvas>
             </div>
             <button
               type="submit"
-              className="bg-teal-500 font-silkscreen shadow-[0_5px_0_rgb(0,0,0)] hover:bg-teal-600 text-white font-semibold py-2 px-4 rounded-md  transition-all ease-out p-2 hover:translate-y-1 hover:shadow-[0_2px_0px_rgb(0,0,0)]">
+              className="bg-teal-500 font-silkscreen shadow-[0_5px_0_rgb(0,0,0)] hover:bg-teal-600 text-white font-semibold py-2 px-4 rounded-md  transition-all ease-out p-2 hover:translate-y-1 hover:shadow-[0_2px_0px_rgb(0,0,0)]"
+            >
               Submit
             </button>
           </div>
--- client/src/views/Game3Page.tsx
+++ client/src/views/Game3Page.tsx
@@ -4,6 +4,23 @@ import { socket } from "../socket/socket";
 export default function Game3Page() {
   const [drawingFromR2, setDrawingFromR2] = useState<string>("");
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const [timer, setTimer] = useState<number>(60); // Timer for 15 seconds
+  const [isTimerRunning, setIsTimerRunning] = useState<boolean>(true);
+
+
+  // Timer logic
+  useEffect(() => {
+    if (isTimerRunning && timer > 0) {
+      const interval = setInterval(() => {
+        setTimer((prev) => prev - 1);
+      }, 1000);
+
+      return () => clearInterval(interval); // Cleanup interval on component unmount or when timer stops
+    } else if (timer === 0) {
+      setIsTimerRunning(false); // Stop the timer when it reaches 0
+      console.log("Time's up!");
+    }
+  }, [isTimerRunning, timer]);
 
   useEffect(() => {
     socket.auth = {
@@ -60,32 +77,40 @@ export default function Game3Page() {
                 <div className="ml-3 text-2xl truncate">Player 2</div>
               </div>
             </div>
-            <button className="mt-4 bg-teal-500 hover:bg-teal-600 text-white font-semibold py-2 px-4 rounded-md shadow-lg">Create New Room</button>
           </div>
         </div>
 
         {/* Right Panel */}
         <div className="w-9/12 bg-white/10 p-6">
           <div className="bg-black bg-opacity-10 p-5 rounded-lg h-full flex flex-col justify-center items-center">
-            <h2 className="text-xl font-bold text-teal-300 mb-4">Game</h2>
+            <h2 className="text-xl font-bold text-teal-300 mb-1">Game</h2>
             <div className="flex-1 w-full flex flex-col items-center justify-center gap-4">
+              {isTimerRunning ? (
+                        <div className="text-xl font-bold text-white">
+                          Time remaining: {timer} seconds
+                        </div>
+                      ) : null}
               <canvas
                 ref={canvasRef}
                 width="800"
                 height="400"
                 className="w-full h-96 bg-white rounded-lg"
-              />
+                />
               {drawingFromR2 ? null : (
-                <p className="text-white">Waiting for the drawing...</p>
+                <p className="text-white"></p>
               )}
+              <div>
+
               <input
                 type="text"
                 placeholder="Guess the word..."
-                className="w-2/3 p-2 rounded-lg text-center border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                className="w-full p-2 rounded-lg text-center border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
-              <button className="bg-teal-500 hover:bg-teal-600 text-white font-semibold py-2 px-4 rounded-md shadow-lg">
+              <button className="bg-teal-500 font-silkscreen shadow-[0_5px_0_rgb(0,0,0)] hover:bg-teal-600 text-white font-semibold py-2 px-4 rounded-md w-full mt-2 transition-all ease-out p-2 hover:translate-y-1 hover:shadow-[0_2px_0px_rgb(0,0,0)] ">
                 Submit Guess
               </button>
+
+              </div>
             </div>
           </div>
         </div>
